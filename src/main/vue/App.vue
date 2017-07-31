a5fb4eb8c43618cc864f2c0f13a8ede0558eeb31
ShanWei <shan.wei1@sf-express.com>
2017-07-31 13:52:52 +0800

    增加导航栏

--- src/main/vue/App.vue
+++ src/main/vue/App.vue
@@ -1,23 +1,46 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div>
+      <div style="float:left;">
+        <img src="./assets/logo.png" style="width:32px;height:32px;">
+      </div>
+
+      <div style="left:32px;float:right;">
+        <el-menu theme="dark"
+                 :default-active="activeIndex"
+                 class="el-menu-demo"
+                 mode="horizontal"
+                 router="true"
+                 @select="handleSelect">
+          <el-menu-item index="1" route="/">Home</el-menu-item>
+          <el-menu-item index="2" route="/counter">Counter</el-menu-item>
+          <el-menu-item index="3" route="/chat">Chat</el-menu-item>
+          <el-menu-item index="4"><a href="http://element.eleme.io" target="_blank">element-ui</a></el-menu-item>
+        </el-menu>
+      </div>
+
+      <div style="clear:both;line-height:0;height:0;font-size:0;overflow:hidden;"></div>
+    </div>
+
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app',
+    data() {
+      return {activeIndex: 1};
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
